450099b50ca2045d5857f644844dac72e2e2a5ae
sherwin-77 <sherwinak1@gmail.com>
2023-08-13 17:28:08 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## SewentySewen Whatsapp BOT
+Another whatsapp (unofficial) bot created with [whatsapp-web.js](https://github.com/pedroslopez/whatsapp-web.js/) for educational purpose
+
+## Quick Start
+Node JS V12+ required to run this
+
+Start the bot by running `npm start` and scan the QR code
+
+## License
+This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+Project [whatsapp-web.js](https://github.com/pedroslopez/whatsapp-web.js/) is licensed under the Apache License, Version 2.0
---
+++ src/index.ts
@@ -0,0 +1,99 @@
+import { Client, LocalAuth, GroupChat } from "whatsapp-web.js";
+import qrcode from "qrcode-terminal";
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        headless: true,
+    }
+})
+
+
+client.on("qr", (qr) => {
+    // Generate and scan this code with your phone
+    qrcode.generate(qr, {small: true}, (code: string) => {
+        console.log(code)
+    });
+});
+
+client.on("authenticated", () => {
+    console.log("AUTHENTICATED");
+});
+
+client.on("auth_failure", (reason) => {
+    console.error("AUTHENTICATION FAILURE", reason);
+});
+
+client.on("ready", () => {
+    console.log("Client is ready!");
+});
+
+// TODO: Subclass?
+client.on("message", async msg => {
+    if (msg.body == "!ping") {
+        await msg.reply('pong');
+    } else if (msg.body.startsWith("!echo")) {
+        if(msg.body.length <= 6) await msg.reply("Message required")
+        else await msg.reply(msg.body.slice(6));
+    } else if (msg.body === '!groupinfo') {
+        const chat = await msg.getChat() as GroupChat
+        if (chat.isGroup) {
+            await msg.reply(`
+            *Group Details*
+            Name: ${chat.name}
+            Description: ${chat.description}
+            Created At: ${chat.createdAt.toString()}
+            Created By: ${chat.owner.user}
+            Participant count: ${chat.participants.length}
+            `);
+            
+        } else {
+            await msg.reply("This command can only be used in a group!");
+        }
+    } else if (msg.body === "!mediainfo" && msg.hasMedia) {
+        const attachmentData = await msg.downloadMedia();
+        await msg.reply(`
+        *Media info*
+        MimeType: ${attachmentData.mimetype}
+        Filename: ${attachmentData.filename}
+        Data (length): ${attachmentData.data.length}
+        `.replace(/^ +/gm, ''));
+    } else if (msg.body === "!resendmedia" && msg.hasQuotedMsg) {
+        const quotedMsg = await msg.getQuotedMessage();
+        if (quotedMsg.hasMedia) {
+            const attachmentData = await quotedMsg.downloadMedia();
+            await client.sendMessage(msg.from, attachmentData, { caption: "Here's your requested media" });
+        }
+        if (quotedMsg.hasMedia && quotedMsg.type === "audio") {
+            const audio = await quotedMsg.downloadMedia();
+            await client.sendMessage(msg.from, audio, { sendAudioAsVoice: true });
+        }
+    } else if (msg.body === "!sticker") {
+        if (msg.type == "image" || msg.type == "video") {
+            const toDownload = await msg.downloadMedia();
+            await client.sendMessage(msg.from, toDownload, { sendMediaAsSticker: true, stickerName: "Sticker", stickerAuthor: "SewentySewenBOT" })
+        } else {
+            const quotedMsg = await msg.getQuotedMessage();
+            if (!(quotedMsg.type == "image" || quotedMsg.type == "video")) return;
+            const toDownload = await quotedMsg.downloadMedia()
+            await client.sendMessage(msg.from, toDownload, { sendMediaAsSticker: true, stickerName: "Sticker", stickerAuthor: "SewentySewenBOT" })
+        }
+        // TODO: Better help command
+    } else if(msg.body === "!help") {
+        await msg.reply(`
+        Hello! I am SewentySewen
+        ~Use "s!help [command] for more info on a command~ (Not supported yet)
+        *Random*
+        ➥ ping, echo, mediainfo, resendmedia
+        *Sticker*
+        ➥ sticker
+        `.replace(/^ +/gm, ''))
+    }
+});
+
+client.on("disconnected", (reason) => {
+    console.log(`Disconnected: ${reason}`)
+})
+
+console.log("Starting")
+client.initialize()
